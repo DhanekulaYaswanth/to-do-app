d04f70bff1cdef08393eb4a0f2735720c8c0c6e6
yaswanth <yaswanth9802@gmail.com>
2023-07-11 22:42:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import './App.css'
+import CreateTODO from "./components/Create";
+import ToDoList from "./components/ToDoList"
+import { getToDoItemsFromLocalStorage } from './Service/Service';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const [ToDoItems,setToDoItems] = useState(getToDoItemsFromLocalStorage('item') || []);
+
+  const [completed,setcompleted] = useState(0)
+
+    
+  
+  return(
+    <div className="maincontainer">
+      <div className="App">
+        <div className="title">
+          <h1>Todo App</h1>
+          <p>Completed: {completed} / {ToDoItems.length} </p>
+        </div>
+        <CreateTODO ToDoItems={ToDoItems} setToDoItems={setToDoItems}/>
+        <ToDoList ToDoItems={ToDoItems} setToDoItems={setToDoItems} completed={completed} setcompleted={setcompleted}/>
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
